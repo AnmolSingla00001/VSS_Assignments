9ef85b66768c492077a6128e216e5f66b774c79e
Anmol Singla <anmolsingla00001@gmail.com>
2021-11-11 08:00:58 +0530

    g-meet

---
+++ Javascript-Practice/js/script.js
@@ -0,0 +1,91 @@
+// const a = "ABC";
+// function fnRandom() {
+//   console.log("Random Function Called.");
+//   console.log(a);
+// }
+// fnRandom;
+
+// setTimeout(() => {}, 5000);
+
+// fetch();
+
+// console.log(variableb); //
+// console.log(variableA); //undefined
+// console.log(abc); //Error
+
+// var variableA = "25"; //Initialised by undefined
+// let variableb = "25"; //not Initialised
+// const variableC = "40";
+// abc = "Some random value";
+
+// function foo() {
+//   let a;
+//   b = 0;
+//   a = b;
+//   a++;
+//   return a;
+// }
+// foo();
+// typeof a;
+// typeof b;
+
+// const profile = {
+//   name: "Anmol",
+//   city: "Singla",
+//   workCity: "Pair",
+//   isInCity: true,
+//   isInWorkCity: false,
+// };
+
+// console.log(checkIfKeyExists(profile, "name"));
+// console.log(checkIfKeyExists(profile, "city"));
+// console.log(checkIfKeyExists(profile, "isInWorkCity"));
+// console.log(checkIfKeyExists(profile, "RandomKey"));
+
+// function checkIfKeyExists(obj, key) {
+//   return obj.hasOwnProperty(key);
+// }
+
+// const clothes = ['jacket', 't-shirt'];
+// clothes.length = 0;
+// clothes[0]; // => ???
+// clothes.length = 50;
+
+// const length = 4;
+// const numbers = [];
+// for (var i = 0; i < length; i++);{
+//   numbers.push(i + 1);
+// }
+// numbers; // => ???
+// [5];
+
+// typeof(typeof(1))
+
+// typeof(typeof(undefined))
+// typeof(typeof(null))
+// typeof(typeof([]))
+// typeof(typeof())
+
+// 'undefined'
+
+// typeof(typeof(!!true))
+// typeof(typeof(1 + "")
+
+// typeof !('true')
+
+// typeof !!('true')
+// typeof !!!("")
+
+// !!!''
+// !!!0
+// !!!null
+// !!!undefined
+// !!!false
+// !!!NaN
+
+// "80" + 71.2
+// 2 + "2"
+
+// 123.967854
+
+// +'1' + +'2'
